57dcaad2d477e7501e8f5d9661e3e66ff3c96ffc
Jackson Reynolds <jwreynold@gmail.com>
2020-07-13 17:44:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Add your code here
+class Dog
+  @@all = []
+
+  def initialize
+    @@all << self
+  end #initialize
+
+  def self.all
+    @@all
+  end #.all
+
+  def self.print_all
+    @@all.each do |dog_instance|
+      puts dog_instance.name
+    end #do
+  end #print_all
+  
+end #Dog
